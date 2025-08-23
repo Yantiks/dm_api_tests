f6d2c7bebae0dec1b1eedf5ee9dc178248ea05af
Yantiks <anaban4na@yandex.ru>
2025-08-23 21:00:58 +0300

    union facades

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -4,6 +4,7 @@ from restclient.configuration import Configuration as DmApiConfiguration
 from restclient.configuration import Configuration as MailHogConfiguration
 from services.api_mailhog import MailhogApi
 from services.dm_api_account import DmApiAccount
+from helpers.account_helper import AccountHelper
 
 structlog.configure(
     processors=[
@@ -18,34 +19,15 @@ def test_login_account():
     account = DmApiAccount(configuration=dm_api_configuration)
     mailhog = MailhogApi(configuration=mailhog_configuration)
 
+    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
+
     # регистрация нового пользователя
-    login = 'yantik_test86'
+    # получение токена с почты
+    # активация токена
+    login = 'yantik_test111'
     password = "12345abcdi"
     email = f'{login}@google.com'
-    json_data = {
-        'login': login,
-        'email': email,
-        'password': password,
-    }
-    response_login = account.account_api.post_v1_account(json_data)
-    assert response_login.status_code == 201, f"Пользователь не был создан, код ошибки: {response_login.status_code}"
-
-    # получение токена с почты
-    response_mail = mailhog.mailhog_api.get_api_v2_messages()
-    assert response_mail.status_code == 200, "Письма не были получены"
-    token = mailhog.mailhog_api.get_activation_token(login=login, response=response_mail)
-    assert token is not None, f"Токен для пользователя {login} не был получен"
-
-    # активация токена
-    activate_token = account.account_api.put_v1_account_token(token=token)
-    assert activate_token.status_code == 200, "Токен не был активирован"
+    account_helper.register_new_user(login=login, password=password, email=email)
 
     # авторизация
-    json_data = {
-        'login': login,
-        'password': password,
-        'rememberMe': True,
-    }
-    try_login = account.login_api.post_v1_account_login(json_data=json_data)
-
-    assert try_login.status_code == 200, f"Пользователь не был авторизован, код ошибки: {try_login.status_code}"
+    account_helper.user_login(login=login, password=password)
--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -4,6 +4,7 @@ from restclient.configuration import Configuration as DmApiConfiguration
 from restclient.configuration import Configuration as MailHogConfiguration
 from services.api_mailhog import MailhogApi
 from services.dm_api_account import DmApiAccount
+from helpers.account_helper import AccountHelper
 
 structlog.configure(
     processors=[
@@ -18,28 +19,12 @@ def test_put_account_token():
     account = DmApiAccount(configuration=dm_api_configuration)
     mailhog = MailhogApi(configuration=mailhog_configuration)
 
+    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
+
     # регистрация нового пользователя
-    login = 'yantik_test83'
+    # получение токена с почты
+    # активация токена
+    login = 'yantik_test113'
     password = "12345abcdi"
     email = f'{login}@google.com'
-    json_data = {
-        'login': login,
-        'email': email,
-        'password': password,
-    }
-    response_login = account.account_api.post_v1_account(json_data)
-    assert response_login.status_code == 201, f"Пользователь не был создан, код ошибки: {response_login.status_code}"
-
-    # получение токена с почты
-    response_mail = mailhog.mailhog_api.get_api_v2_messages()
-    assert response_mail.status_code == 200, "Письма не были получены"
-    token = mailhog.mailhog_api.get_activation_token(login=login, response=response_mail)
-    print(f'token: {token}')
-    print(response_mail.status_code)
-    assert token is not None, f"Токен для пользователя {login} не был получен"
-
-    # активация токена
-    activate_token = account.account_api.put_v1_account_token(token=token)
-    print(f'activate_token: {activate_token.json()}')
-    print(activate_token.status_code)
-    assert activate_token.status_code == 200, "Токен не был активирован"
+    account_helper.register_new_user(login=login, password=password, email=email)
