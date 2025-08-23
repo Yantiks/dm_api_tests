3d65a2adf5cf646549a5c99ee2790bf943b3f632
Yantiks <anaban4na@yandex.ru>
2025-08-23 19:35:21 +0300

    simple facade added
    # Conflicts:
    #	tests/functional/post_v1_account/test_post_v1_account.py
    #	tests/functional/post_v1_account_login/test_post_v1_account_login.py

--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -1,9 +1,9 @@
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
-from api_mailhog.apis.mail_api import MailApi
+
 import structlog
 from restclient.configuration import Configuration as DmApiConfiguration
 from restclient.configuration import Configuration as MailHogConfiguration
+from services.api_mailhog import MailhogApi
+from services.dm_api_account import DmApiAccount
 
 structlog.configure(
     processors=[
@@ -15,9 +15,8 @@ def test_login_with_changed_email():
     mailhog_configuration = MailHogConfiguration(host='http://5.63.153.31:5025')
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    account_api = AccountApi(configuration=dm_api_configuration)
-    login_api = LoginApi(configuration=dm_api_configuration)
-    mail_api = MailApi(configuration=mailhog_configuration)
+    account = DmApiAccount(configuration=dm_api_configuration)
+    mailhog = MailhogApi(configuration=mailhog_configuration)
 
     # регистрация нового пользователя
     login = 'yantik_test127'
@@ -28,17 +27,17 @@ def test_login_with_changed_email():
         'email': email,
         'password': password,
     }
-    response_login = account_api.post_v1_account(json_data)
+    response_login = account.account_api.post_v1_account(json_data)
     assert response_login.status_code == 201, f"Пользователь не был создан, код ошибки: {response_login.status_code}"
 
     # получение токена с почты
-    response_mail = mail_api.get_api_v2_messages()
+    response_mail = mailhog.mailhog_api.get_api_v2_messages()
     assert response_mail.status_code == 200, "Письма не были получены"
-    token = mail_api.get_activation_token(login=login, response=response_mail)
+    token = mailhog.mailhog_api.get_activation_token(login=login, response=response_mail)
     assert token is not None, f"Токен для пользователя {login} не был получен"
 
     # активация токена
-    activate_token = account_api.put_v1_account_token(token=token)
+    activate_token = account.account_api.put_v1_account_token(token=token)
     assert activate_token.status_code == 200, "Токен не был активирован"
 
     # изменение почты пользователя
@@ -48,7 +47,7 @@ def test_login_with_changed_email():
         'email': "new" + email,
     }
 
-    change_email = account_api.put_v1_account_email(json_data=json_data_new)
+    change_email = account.account_api.put_v1_account_email(json_data=json_data_new)
     assert change_email.status_code == 200, f"Почта для пользователя {login} не была изменена"
 
     # попытка авторизации со старым токеном
@@ -57,17 +56,17 @@ def test_login_with_changed_email():
         'password': password,
         'rememberMe': True,
     }
-    try_enter = login_api.post_v1_account_login(json_data=json_data)
+    try_enter = account.login_api.post_v1_account_login(json_data=json_data)
     assert try_enter.status_code == 403, f"Пользователь был авторизован, код ответа: {try_enter.status_code}"
 
     # получение нового токена с почты
-    response_mail = mail_api.get_api_v2_messages()
+    response_mail = mailhog.mailhog_api.get_api_v2_messages()
     assert response_mail.status_code == 200, "Письма не были получены"
-    token = mail_api.get_activation_token(login=login, response=response_mail)
+    token = mailhog.mailhog_api.get_activation_token(login=login, response=response_mail)
     assert token is not None, f"Токен для пользователя {login} не был получен"
 
     # активация нового токена
-    activate_token = account_api.put_v1_account_token(token=token)
+    activate_token = account.account_api.put_v1_account_token(token=token)
     assert activate_token.status_code == 200, "Токен не был активирован"
 
     # логин с новым токеном
@@ -76,5 +75,5 @@ def test_login_with_changed_email():
         'password': password,
         'rememberMe': True,
     }
-    try_enter = login_api.post_v1_account_login(json_data=json_data)
+    try_enter = account.login_api.post_v1_account_login(json_data=json_data)
     assert try_enter.status_code == 200, f"Пользователь не был авторизован, код ошибки: {try_enter.status_code}"
