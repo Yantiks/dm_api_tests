335c7b43cb6c7d3f71a445819f642ed546d81d6a
Yantiks <anaban4na@yandex.ru>
2025-09-04 12:09:14 +0300

    add parametrized test

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,4 +1,7 @@
+import pytest
 import structlog
+
+from checkers.http_checkers import check_status_code_http
 
 structlog.configure(
     processors=[
@@ -6,9 +9,31 @@ structlog.configure(
     ]
 )
 
+
 def test_account_creation(account_helper, prepare_user):
     # регистрация нового пользователя
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
     account_helper.create_user(login=login, password=password, email=email)
+
+
+@pytest.mark.parametrize(
+    "login, email, password, expected_status_code, error_message",
+    [
+        # 1. Короткий пароль
+        ("validlogin5", "user5@example.com", "123", 400, "Validation failed"),
+
+        # 2. Невалидный email
+        ("validlogin6", "invalidemail.com", "validPass123", 400, "Validation failed"),
+
+        # 3. Невалидный логин
+        ("a", "user6@example.com", "validPass123", 400, "Validation failed"),
+    ]
+)
+def test_post_v1_account_negative(account_helper, login, email, password, expected_status_code, error_message):
+    login = login
+    password = password
+    email = email
+    with check_status_code_http(expected_status_code, error_message):
+        account_helper.create_user(login=login, password=password, email=email)
